9f7f5441e49146f4d243aa2a971e5813305a1118
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2014-04-03 23:53:45 +0000

    rename rose radius parameter to offset; correct note about double-tracing rose curves

--- anim_rose.py
+++ anim_rose.py
@@ -8,16 +8,16 @@
 import math
 import anim_common
 
-def draw(g, radius, amplitude, freq, phase, nr_steps) :
+def draw(g, amplitude, freq, offset, phase, nr_steps) :
     # note freq must be a Fraction
 
     def curve_func(x) :
-        # Note that the curve can still be traced twice in some situations, e.g.
-        # try a frequency of 3/5 and a radius of 0. But if the radius is set to nonzero,
-        # the two halves no longer overlap.
+        # Note that the curve can still be traced twice in some situations, namely where
+        # the frequency numerator and denominator are both odd, and the offset is zero.
+        # But if the offset is set to nonzero, the two halves no longer overlap.
         phi = 2 * math.pi * x * freq.denominator
         theta = 2 * math.pi * (x + phase) * freq.numerator
-        r = radius + math.sin(theta) * amplitude
+        r = offset + math.sin(theta) * amplitude
         return \
             (r * math.cos(phi), r * math.sin(phi))
     #end curve_func
@@ -25,9 +25,9 @@ def draw(g, radius, amplitude, freq, phase, nr_steps) :
     anim_common.draw_curve(g, f = curve_func, closed = True, nr_steps = nr_steps)
 #end draw
 
-def make_draw(radius, amplitude, freq, phase, nr_steps, do_settings = None) :
+def make_draw(amplitude, freq, offset, phase, nr_steps, do_settings = None) :
     # note freq must be a Fraction
-    radius = anim_common.ensure_interpolator(radius)
+    offset = anim_common.ensure_interpolator(offset)
     amplitude = anim_common.ensure_interpolator(amplitude)
     freq = anim_common.ensure_interpolator(freq)
     phase = anim_common.ensure_interpolator(phase)
@@ -41,7 +41,7 @@ def make_draw(radius, amplitude, freq, phase, nr_steps, do_settings = None) :
         draw \
           (
             g = g,
-            radius = radius(x),
+            offset = offset(x),
             amplitude = amplitude(x),
             freq = freq(x),
             phase = phase(x),
